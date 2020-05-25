6553720ac7d258274bc266e915c3be172d2ed53a
sangeetarao <40835153+sangeetarao@users.noreply.github.com>
2020-05-26 01:14:59 +0530

    MC method without acceptance or rejection
    A

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import random 
 import math
+from operator import add 
+from functools import reduce
 #Defining constants 
 a=5.3*(10**-10)
 sigma=3.4*(10**-10)
 epsilon=10*(10**3)
+alpha=0.2
 #Generating N=500 data using fcc base
 def create_N_particles( n ):
     x=(n/4)**(1/3)
@@ -21,7 +24,10 @@ def create_N_particles( n ):
     return particle_position
 
 def calculate_distance(ri,rj):
-    rij=((ri[0]-rj[0])**2+(ri[1]-rj[1])**2+(ri[2]-rj[2]**2))**0.5
+    a=(ri[0]-rj[0])**2
+    b=(ri[1]-rj[1])**2
+    c=(ri[2]-rj[2])**2
+    rij=math.sqrt(a+b+c)
     return rij
 
 def calc_energy(r):
@@ -31,23 +37,47 @@ def calc_energy(r):
 def average_energy(lst): 
     return reduce(lambda a, b: a + b, lst) / len(lst) 
 
+def r_prime_calc(r):
+    n1=((random.random()*2)-1)*alpha
+    n2=((random.random()*2)-1)*alpha
+    n3=((random.random()*2)-1)*alpha
+    random_nums=[n1,n2,n3]
+    r_prime=list(map(add, r, random_nums))
+    return r_prime
 
 
 particle_position=create_N_particles(500)
-print(len(particle_position))
-sample_list=[[0,0,0],[0,1,0],[0,0,1],[1,0,0]]
-changes=[]
+change=[]
 energy=[]
-alpha=0.2
-# Starting MC Method trial 
-ri=sample_list[0]
-rj=sample_list[1]
-rij=calculate_distance(ri,rj)
-energy.append(calc_energy(rij))
 
-print(energy)
-n1=(random.random()*2)-1
-n2=(random.random()*2)-1
-n3=(random.random()*2)-1
+# # for first particle 
+# r=particle_position[0]
+# for i in range(0,5):
+#     r_prime=r_prime_calc(r)
+#     for j in particle_position:
+#         if j!=r:
+#             rij=calculate_distance(r,j)
+#             if float(rij)<(3*sigma):
+#                 phi_rij=calc_energy(rij)
+#                 energy.append(phi_rij)
+#     i=i+1
 
-print(n1,n2,n3)
+#     avg=average_energy(energy)
+#     change.append([r,r_prime,avg])
+# print(change)
+                
+
+for r in particle_position:
+    for i in range(0,5):
+        r_prime=r_prime_calc(r)
+        for j in particle_position:
+            if j!=r:
+                rij=calculate_distance(r,j)
+                if float(rij)<(3*sigma):
+                    phi_rij=calc_energy(rij)
+                    energy.append(phi_rij)
+        avg=average_energy(energy)
+        change.append([r,r_prime,avg])
+print(len(change))
+
+
