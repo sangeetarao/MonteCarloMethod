5bab3995ffc0d37037a63bffbbb82e59c585867d
sangeetarao <40835153+sangeetarao@users.noreply.github.com>
2020-06-01 19:47:13 +0530

    Algorithm for sequential MC Complete

--- sequential_MC.py
+++ sequential_MC.py
@@ -1,10 +1,11 @@
 import random 
 import math
 from operator import add 
-from functools import reduce
 import matplotlib.pyplot as plt 
+import time
+start=time.time()
 
-#Generating N=500 data using fcc base
+#Generating N=500 data using fcc lattice
 def create_N_particles( n ):
     x=int((n/4)**(1/3))
     particle_position=[]
@@ -21,113 +22,159 @@ def create_N_particles( n ):
     return particle_position
 
 def calculate_distance(ri,rj):
-    a=(ri[0]-rj[0])**2
-    b=(ri[1]-rj[1])**2
-    c=(ri[2]-rj[2])**2
-    rij=math.sqrt(a+b+c)
-    return rij
+	dx=(to_make_bulk(ri[0],rj[0]))**2
+	dy=(to_make_bulk(ri[1],rj[1]))**2
+	dz=(to_make_bulk(ri[2],rj[2]))**2
+	rij=math.sqrt(dx+dy+dz)
+	return rij
+
+def to_make_bulk(i,j):
+	dx=i-j 
+	if dx>L/2:
+		dx=dx-L 
+	if dx<(-L/2):
+		dx=dx+L 
+	return dx
 
 def calc_energy(r):
-    phi_rij=4*epsilon*(((sigma/r)**12)-((sigma/r)**6))
-    return phi_rij
+	constant=(sigma/r)**6
+	phi_rij=epsilon_4*((constant**2)-constant)
+	return phi_rij
 
 def r_prime_calc(r):
     n1=((random.random()*2)-1)*alpha
     n2=((random.random()*2)-1)*alpha
     n3=((random.random()*2)-1)*alpha
     random_nums=[n1,n2,n3]
-    r_prime=list(map(add, r, random_nums))
+    r_prime=pbc(list(map(add, r, random_nums)))
     return r_prime
-def calc_acceptance(lst):
-	acceptance_rate=sum(lst)/len(lst)
-	return acceptance_rate
 
-
+def pbc(r):
+	if r[0]>L:
+		r[0]=r[0]-L 
+	elif r[0]<0:
+		r[0]=r[0]+L
+	if r[1]>L:
+		r[1]=r[1]-L 
+	elif r[1]<0:
+		r[1]=r[1]+L
+	if r[2]>L:
+		r[2]=r[2]-L 
+	elif r[2]<0:
+		r[2]=r[2]+L
+	return r
 
 
 #Defining constants and initializing 
 a=5.3*(10**-10)
+L=5*a
 sigma=3.4*(10**-10)
 epsilon=10**3
+epsilon_4=4*epsilon
 alpha=0.3*(10**-10) #should be around 0.3 A
 R=8.3145
 T=100
-e=2.718281828
 particle_position=create_N_particles(500)
 energy_original=[]
-acceptance=[]
+acceptance=0
 Ui=[]
-configurations=[]
-mc_cycle=100
+mc_cycle=5000
+x=[]
+y=[]
 #MC METHOD
-test=particle_position.copy()
-#Energy of system for base configuration
-for ri in test:
-    energy_original=[]
-    for j in test:
-        if ri!=j:
-            rij=calculate_distance(ri,j)
-            if rij<(3*sigma):
-                energy_original.append(calc_energy(rij))
-    Ui.append(sum(energy_original))
-U=sum(Ui)/500
-U_run=U
-print("Energy of base configuration is: ", U ,"J/mol")
 
-eng_og=[]
-eng_prime=[]
-check=0
-#Starting MC Cycles
-for imc in range(0,mc_cycle):
-	for ri in test:
-		eng_og=[]
-		eng_prime=[]
-		for j in test:
+#Energy of system for base configuration
+for ri in particle_position:
+	energy_original=[]
+	for j in particle_position:
 			if ri!=j:
 				rij=calculate_distance(ri,j)
 				if rij<(3*sigma):
-				    eng_og.append(calc_energy(rij))
-		Uri=sum(eng_og)
+					energy_original.append(calc_energy(rij))
+	Ui.append(sum(energy_original))
+U=sum(Ui)
+U_run=U
+print("Energy of base fcc configuration is: ", (U/500)/1000,"KJ/mol")
+U_test=0
+U_test_n=0
+#Code with neighbours 
+
+
+#Starting MC Cycles
+for imc in range(0,mc_cycle):
+	#To calculate neighbours for every 10 cycles
+	if imc%10==0:
+		neighbours=[]
+		for ri in particle_position:
+			n_for_ri=[]
+			for j in particle_position:
+				if ri!=j:
+					rij=calculate_distance(ri,j)
+					if rij<(3*sigma)+(1*(10**-10)):
+						n_for_ri.append(particle_position.index(j))
+			neighbours.append(n_for_ri)
+		print("--------------------------------------------------------------------------------------------------------------------------------")
+
+	#To calculate energy of system before moving particle
+	energy_of_system=[]
+	for ri in particle_position:
+		eng_og=[]
+		eng_prime=[]
+		n_index=particle_position.index(ri)
+		for j_index in neighbours[n_index]:
+			j=particle_position[j_index]
+			if ri!=j:
+				rij=calculate_distance(ri,j)
+				if rij<(3*sigma):
+					eng_og.append(calc_energy(rij))
+		Uri=(sum(eng_og))
+		energy_of_system.append(Uri)
+		#Generating r prime
 		ri_prime=r_prime_calc(ri)
-		for j in test:
-			if j!=ri:
+		for j_index in neighbours[n_index]:
+			j=particle_position[j_index]
+			if ri!=j:
 				rij_prime=calculate_distance(ri_prime,j)
 				if rij_prime<(3*sigma):
-				    eng_prime.append(calc_energy(rij_prime))
-		Uri_prime=sum(eng_prime)
+					eng_prime.append(calc_energy(rij_prime))
+		#energy of system with r prime (after move)
+		Uri_prime=(sum(eng_prime))
 		delta=Uri_prime-Uri
-		# print(delta)
-		if Uri_prime<Uri:
-			acceptance.append(1)
-			check=1
+		#if move is accepted then change Urun by adding delta
+		if Uri_prime<U_run:
+			acceptance=acceptance+1
+			index=particle_position.index(ri)
+			particle_position[index]=ri_prime
+			U_run+=(delta)
 		else:
 			P = math.exp(-delta/(R*T))
-			G = (random.random())*2-1
+			G = (random.random())
 			if G<P:
-				acceptance.append(1)
-				check=1
+				acceptance=acceptance+1
+				index=particle_position.index(ri)
+				particle_position[index]=ri_prime
+				U_run+=(delta)
+			#Reject if move is not accepted
 			else:
-				acceptance.append(0)
-				check=0
-		if check==1:
-			index=test.index(ri)
-			test[index]=ri_prime
-			U_run=U_run+delta
-	if (imc%10)==0:
-		alpha_test=calc_acceptance(acceptance)
+				pass
+	#to calculate new alpha based on average acceptance rate 
+	x.append(imc)
+	y.append(U_run/500)
+	alpha_test=acceptance/((imc+1)*500)
+	if (imc!=0 and imc%10==0):
+		# print("Energy of system(wrt delta)",(U_run/500)/1000,"Energy of system by calculation",(sum(energy_of_system)/500)/1000)
 		if alpha_test<0.5:
 			alpha=alpha-(0.01*(10**-10))
-			print("Changed alpha to: ",alpha)
+			print("Changed alpha to:",alpha,"Energy of system(wrt delta)",(U_run/500)/1000,"Energy of system by calculation",(sum(energy_of_system)/500)/1000)
 		elif alpha_test>0.5:
 			alpha=alpha+(0.01*(10**-10))
-			print("Changed alpha to: ",alpha)
-	configurations.append(test)
-	print("MC Cycle: ",imc+1," Energy:", U_run," Acceptance:",calc_acceptance(acceptance))
-
-
-print("Total acceptance is:",calc_acceptance(acceptance),"Final alpha value:",alpha)
-
-        
-
+			print("Changed alpha:",alpha)
+	print("MC Cycle:",imc)
+	print("\tTotal Energy (Average for ith particle):",(U_run/1000)/500,"KJ/mol")
+end=time.time()
+print("\nTotal acceptance is:",alpha_test,"Final alpha value:",alpha,"Time taken: ",end-start)
+#To plot energy vs mc cycle 
+plt.plot(x,y)
+plt.show()
 
 
